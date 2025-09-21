39ad3ff662e0a1fc7e985e616c082e87b4666312
fumin01@snu.ac.kr <fumin01@snu.ac.kr>
2025-09-21 14:21:28 +0900

    Complete all custom errors

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Request
 from fastapi.exceptions import RequestValidationError
+from fastapi.response import JSONResponse
 
 from tests.util import get_all_src_py_files_hash
 from src.api import api_router
+
+from src.users.schemas import CreateUserRequest, UserResponse
+from src.users.errors import *
 
 app = FastAPI()
 
@@ -21,3 +25,37 @@ def health_check():
         "status": "ok",
         "hash": hash
     }
+
+user_db = {}
+
+# /api/users 엔드포인트
+@app.post("/api/users/", status_code=status.HTTP_200_OK)
+def create_user(request: CreateUserRequest) -> UserResponse:
+    user_id = len(user_db) + 1
+    user_db[user_id] = request.model_dump()
+
+    # check is there any repeated Email
+    for user in user_db:
+        if user["email"] == request.email:
+            raise InvalidEmailException()
+
+    return UserResponse(
+        user_id=user_id,
+        name=request.name,
+        email=request.email,
+        password=request.password,
+        phone_number=request.phone_number,
+        height=request.height,
+        bio=request.bio
+    )
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    custom_exc = MissingValueException()
+    return JSONResponse(
+        status_code=custom_exc.status_code,
+        content={
+            "error_code": custom_exc.error_code,
+            "error_message": custom_exc.error_message
+        }
+    )
