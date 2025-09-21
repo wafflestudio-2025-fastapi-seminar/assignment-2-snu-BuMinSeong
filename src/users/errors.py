0dd62156997c6768ea41a45e8ce1f52a32a9e711
fumin01@snu.ac.kr <fumin01@snu.ac.kr>
2025-09-21 11:36:23 +0900

    error customizing

--- src/users/errors.py
+++ src/users/errors.py
@@ -1,9 +1,41 @@
 from common import CustomException
 
+class MissingValueException(CustomException):
+    def __init__(self):
+        super().__init__(
+            status_code=422,
+            error_code="ERR_001",
+            error_message="MISSING VALUE"
+        )
+
 class InvalidPasswordException(CustomException):
     def __init__(self):
         super().__init__(
             status_code=422,
             error_code="ERR_002",
             error_message="INVALID PASSWORD"
+        )
+
+class InvalidPhonenumberException(CustomException):
+    def __init__(self):
+        super().__init__(
+            status_code=422,
+            error_code="ERR_003",
+            error_message="INVALID PHONE NUMBER"
+        )
+
+class InvalidBioException(CustomException):
+    def __init__(self):
+        super().__init__(
+            status_code=422,
+            error_code="ERR_004",
+            error_message="BIO TOO LONG"
+        )
+
+class invalidEmailException(CustomException):
+    def __init__(self):
+        super().__init__(
+            status_code=422,
+            error_code="ERR_005",
+            error_message="EMAIL ALREADY EXISTS"
         )
